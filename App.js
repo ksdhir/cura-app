9756dfadc24a56cb4a8e5678625f43751ff265fb
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-09-18 18:02:20 -0700

    fetch and display steps from Health app

--- App.js
+++ App.js
@@ -1,10 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AppleHealthKit, {
+  HealthValue,
+  HealthKitPermissions,
+} from "react-native-health";
+
+const HealthMetrics = ({ label, value }) => {
+  return (
+    <View>
+      <Text>
+        {" "}
+        {label} of User: {value}{" "}
+      </Text>
+    </View>
+  );
+};
+
+const permissions = {
+  permissions: {
+    read: [
+      AppleHealthKit.Constants.Permissions.HeartRate,
+      AppleHealthKit.Constants.Permissions.Steps,
+    ],
+    write: [AppleHealthKit.Constants.Permissions.Steps],
+  },
+};
 
 export default function App() {
+  const [hasPermissions, setHasPermissions] = useState(false);
+  const [steps, setSteps] = useState(0);
+
+  useEffect(() => {
+    AppleHealthKit.initHealthKit(permissions, (error) => {
+      /* Called after we receive a response from the system */
+
+      if (error) {
+        console.log("[ERROR] Cannot grant permissions!");
+      }
+
+      setHasPermissions(true);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (hasPermissions) {
+      /* Can now read or write to HealthKit */
+
+      const options = {
+        startDate: new Date(2020, 1, 1).toISOString(),
+      };
+
+      // AppleHealthKit.getHeartRateSamples(
+      //   options,
+      //   (callbackError, results) => {
+      //     /* Samples are now collected from HealthKit */
+      //   },
+      // )
+
+      AppleHealthKit.getStepCount(options, (err, steps) => {
+        console.log("Steps", steps);
+        setSteps(steps.value);
+      });
+    }
+  }, [hasPermissions]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <HealthMetrics label="Distance" value="4km" />
+      <HealthMetrics label="Steps" value={steps.toString()} />
+      <HealthMetrics label="Climb" value="456" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +78,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "left",
+    justifyContent: "center",
   },
 });
