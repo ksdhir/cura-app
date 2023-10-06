e807a0f756d15923747d98f468ad2e2db0cbcd77
JB Sinluenam <jb.sinluenam@gmail.com>
2023-10-06 10:23:08 -0700

    change frontend configurations

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { NavigationContainer } from "@react-navigation/native";
-import AppNavigator from "./src/navigation/AppNavigator";
-import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './src/navigators/AppNavigator';
+
+import React from 'react';
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <AppNavigator/>
+    </NavigationContainer>
+  );
 }
