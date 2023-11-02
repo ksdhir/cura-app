1c314e3a78c00adcc3d3da2b148bd10810ff4892
JB Sinluenam <jb.sinluenam@gmail.com>
2023-11-02 13:11:28 -0700

    feat: #CURA-174-Fix header on main screen

--- src/screens/HeartRate/HeartRateMainScreen.js
+++ src/screens/HeartRate/HeartRateMainScreen.js
@@ -16,6 +16,7 @@ import curaTheme from "../../theme/theme";
 import Graph from "../../assets/icons/svg/graph.svg";
 import useAuth from "../../hooks/useAuth";
 import Header from "../../components/layouts/Header";
+import IconBtn from "../../components/IconBtn";
 
 //TODO:Fetching
 //1. getElderEmail from caregiver profile by pass in user.email
@@ -113,17 +114,28 @@ export default function HeartRateMainScreen() {
     <SafeAreaView className="flex-1 items-center justify-center px-4 bg-curaWhite">
       <StatusBar style="auto" />
       {/* <Header /> */}
-      <View className="w-full justify-center mt-3">
-        {userLoggedIn === "Caregiver" ? (
-          <Text className=" text-xl text-curaBlack font-bold">{elderName}</Text>
-        ) : (
-          <Text className=" text-xl text-curaBlack font-bold">
-            Hello {elderName}
+      <View className="w-full flex-row justify-between py-4">
+        <View>
+          {userLoggedIn === "Caregiver" ? (
+            <Text className=" text-xl text-curaBlack font-bold">
+              {elderName}
+            </Text>
+          ) : (
+            <Text className=" text-xl text-curaBlack font-bold">
+              Hello {elderName}
+            </Text>
+          )}
+          <Text className=" text-base text-curaBlack font-medium ">
+            {elderAge} years old
           </Text>
-        )}
-        <Text className=" text-base text-curaBlack font-medium ">
-          {elderAge} years old
-        </Text>
+        </View>
+        <IconBtn
+          name="bell"
+          onPress={() => navigation.navigate("NotificationHistory")}
+          iconStyle={{
+            color: curaTheme.lightColors.primary,
+          }}
+        />
       </View>
       <Image
         className=" flex-1 justify-start w-full relative -z-10 top-4"
