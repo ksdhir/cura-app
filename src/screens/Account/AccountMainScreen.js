095206bd6022f6a04b64c0701c4d742c75596891
haidoe <haidren.amalia@gmail.com>
2023-11-01 17:32:06 -0700

    feat: account main screen layout

--- src/screens/Account/AccountMainScreen.js
+++ src/screens/Account/AccountMainScreen.js
@@ -1,18 +1,15 @@
-import {
-  View,
-  Text,
-  TextInput,
-  Pressable,
-  ScrollView,
-  TouchableOpacity,
-} from "react-native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import auth from "@react-native-firebase/auth";
 import useAuth from "../../hooks/useAuth";
+import Header from "../../components/layouts/Header";
+import UserIcon from "../../assets/icons/svg/avatar.svg";
+import SettingIcon from "../../assets/icons/svg/setting.svg";
 
 export default function AccountMainScreen() {
   const navigation = useNavigation();
@@ -42,27 +39,60 @@ export default function AccountMainScreen() {
   return (
     <SafeAreaView className="flex flex-1 bg-curaWhite">
       <StatusBar style="auto" />
+
+      <View className="px-4">
+        <Header />
+      </View>
+
       <View className="flex w-full flex-1 justify-center items-center space-y-4">
-        <Text className="text-2xl text-neutral-800 font-bold mt-4">
-          Account Main Screen
-        </Text>
-        <Text className="text-4xl mt-4">
-          Hi {user.preferredName || user.email}
-          {/* {user.preferredName
-            ? user.preferredName.length > 10
-              ? user.preferredName.substring(0, 10) + "..."
-              : user.preferredName
-            : user.email.length > 10
-            ? user.email.substring(0, 10) + "..."
-            : user.email} */}
-        </Text>
-        <TouchableOpacity
-          className="py-3 bg-indigo-500 rounded-xl p-4"
-          onPress={handleSignout}
-        >
-          <Text className="text-xl font font-bold text-center text-gray-200">
-            Sign out
+        <View className="w-full flex flex-row  items-center justify-start gap-2 border-b-[1px] pb-4 px-4 mt-4">
+          <UserIcon width={20} height={20} style={{ color: "#323333" }} />
+
+          <Text className="text-lg text-neutral-800 font-bold">Account</Text>
+        </View>
+
+        <View className="w-full flex flex-row  items-center justify-between gap-2 px-4">
+          <Text className="text-lg text-neutral-800 font-bold">
+            Personal Profile
           </Text>
+
+          <MaterialCommunityIcons name={"chevron-right"} size={24} />
+        </View>
+
+        <View className="w-full flex flex-row  items-center justify-between gap-2 px-4 mb-4">
+          <Text className="text-lg text-neutral-800 font-bold">
+            View QR Code
+          </Text>
+
+          <MaterialCommunityIcons name={"chevron-right"} size={24} />
+        </View>
+
+        <View className="w-full flex flex-row  items-center justify-start gap-2 border-b-[1px] pb-4 px-4 mt-4">
+          <SettingIcon width={20} height={20} style={{ color: "#323333" }} />
+
+          <Text className="text-lg text-neutral-800 font-bold">Settings</Text>
+        </View>
+
+        <View className="w-full flex flex-row items-center justify-between gap-2 px-4">
+          <Text className="text-lg text-neutral-800 font-bold">
+            Notifications
+          </Text>
+
+          <MaterialCommunityIcons name={"chevron-right"} size={24} />
+        </View>
+
+        <View className="w-full flex flex-row  items-center justify-between gap-2 px-4">
+          <Text className="text-lg text-neutral-800 font-bold">
+            Heart Rate Threshold
+          </Text>
+
+          <MaterialCommunityIcons name={"chevron-right"} size={24} />
+        </View>
+
+        <TouchableOpacity onPress={handleSignout} className="w-full p-4">
+          <View className="flex flex-row  items-center justify-between gap-2 ">
+            <Text className="text-lg text-neutral-800 font-bold">Logout</Text>
+          </View>
         </TouchableOpacity>
 
         <ScrollView
