47f22aceadf9d6b94fe8c42f4968637aa5ff6f0f
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-11-08 23:21:31 -0800

    chore: CURA-181 remove old sync method

--- App.js
+++ App.js
@@ -9,42 +9,7 @@ import "expo-dev-client";
 import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
 
-import {
-  locationPermission,
-  getLocationCoords,
-  runLocationDetectionProcess,
-} from "./src/hooks/locationPermission";
-
 export default function App() {
-
-  const isPermissionsOn = false
-  const [isOnce, setIsOnce] = useState(false);
-
-  useEffect(() => {
-    // call for permission after 2 seconds of loading of app
-
-    if (!isOnce && isPermissionsOn) {
-      setTimeout(() => {
-        console.log("running locationdetection");
-        initiatePermission();
-        setIsOnce(true);
-      }, 2000);
-    }
-  }, []);
-
-  function initiatePermission() {
-    // Location Permissions
-    locationPermission()
-      .then((permission) => {
-        if (permission) {
-          return getLocationCoords();
-        }
-      })
-      .then((coordsObj) => {
-        console.log(coordsObj, "coords object");
-        runLocationDetectionProcess();
-      });
-  }
 
   const [fontsLoaded] = useFonts({
     SatoshiMedium: require("./src/assets/fonts/Satoshi-Medium.otf"),
